33e9baa0c9f8081369596c6c7721d9776d91ee86
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-12-27 20:20:53 +0900

    デモをChromeから変更

--- background.js
+++
@@ -1,8 +0,0 @@
-
-chrome.browserAction.onClicked.addListener(function (tab){
-	var url = chrome.extension.getURL("options.html");
-	var createProperties = {
-		"url": url
-	};
-	chrome.tabs.create(createProperties);
-});
